c5c990618261565375b7c84e9bc8c6e708acc139
somyalakshmi07 <somyalakshmi18@gmail.com>
2025-07-19 15:35:23 +0530

    mysql connector ok

--- login/app1.py
+++ login/app1.py
@@ -331,5 +331,5 @@ def export_summary():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8080))   # ← use the env‑supplied port
+    port = int(os.environ.get("PORT", 3306))   # ← use the env‑supplied port
     app.run(host="0.0.0.0", port=port)
