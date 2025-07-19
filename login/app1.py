0692c5b6cea8b81a657ea0d760237a6066f730aa
somyalakshmi07 <somyalakshmi18@gmail.com>
2025-07-19 12:16:36 +0530

    chnages in req

--- login/app1.py
+++ login/app1.py
@@ -318,5 +318,7 @@ def export_summary():
         return f"An error occurred during export: {str(e)}"
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
